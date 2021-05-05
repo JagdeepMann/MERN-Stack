3053aca0a20db0f9d408c9162d01af24138aac60
JagdeepMann <jagmann1101@gmail.com>
2021-05-05 17:31:03 -0500

    fixed bugs in propItUP

--- React/propItUP/src/App.js
+++ React/propItUP/src/App.js
@@ -5,35 +5,36 @@ import PersonCard from './components/PersonCard';
 
 function App() {
   return (
-    <>
-    <div className="App">
-      <div>
-          <PersonCard 
-          fName="Jane"
-          lName="Doe"
-          age={45}
-          hairColor="Black"/>
 
-          <PersonCard 
-          fName="John"
-          lName="Smith"
-          age={45}
-          hairColor="Brown"/>
+      <div className="App">
+        <div>
+            <PersonCard
+            fName={"Jane"} lName={"Doe"} 
+            age={45}
+            hairColor={"Black"}>
+            </PersonCard>
 
-          <PersonCard 
-          fName="Fillmore"
-          lName="Millard"
-          age={50}
-          hairColor="Brown"/>
+            
+            <PersonCard 
+            fName={"John"} lName={"Smith"}
+            age={45}
+            hairColor={"Brown"}>
+            </PersonCard>
 
-          <PersonCard 
-          fName="Smith"
-          lName="Maria"
-          age={62}
-          hairColor="Brown"/>
+            <PersonCard 
+            fName={"Fillmore"} lName={"Millard"}
+            age={50}
+            hairColor={"Brown"}>
+            </PersonCard>
+
+            <PersonCard 
+            fName={"Smith"} lName={"Maria"}
+            age={62}
+            hairColor={"Brown"}>
+            </PersonCard>
+        </div>
       </div>
-    </div>
-    </>
+
   );
 }
 
